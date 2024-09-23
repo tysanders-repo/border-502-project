7424df2e03f5cbd3623d0a440c0368cb867beef2
Gemma Goddard <142944182+GemmaGoddard33@users.noreply.github.com>
2024-09-23 10:54:47 -0500

    Trying to fix jest imports

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -10,5 +10,7 @@ module.exports = {
       '^@pages/(.*)$': '<rootDir>/src/pages/$1',
       '^@components/(.*)$': '<rootDir>/src/components/$1',
       '^@services/(.*)$': '<rootDir>/src/services/$1',
+      '^@utils/(.*)$': '<rootDir>/src/utils/$1',
     },
+
   };
