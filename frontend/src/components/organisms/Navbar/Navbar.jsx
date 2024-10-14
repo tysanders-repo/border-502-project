b172f7e4be6310ecc37783e024fa54d6bafbab40
derian-c <magicalhippo77@gmail.com>
2024-10-14 08:17:58 -0500

    Test other environment variables

--- frontend/src/components/organisms/Navbar/Navbar.jsx
+++ frontend/src/components/organisms/Navbar/Navbar.jsx
@@ -130,6 +130,7 @@ export default function Navbar() {
   }, []);
   console.log(process.env.NEXTAUTH_URL);
   console.log(process.env.REACT_APP_API_URL);
+  console.log(process.env.NEXT_PUBLIC_AUTHJS_LOCAL_SESSION_COOKIE);
   console.log("Next line");
   return (
     <AppBar
