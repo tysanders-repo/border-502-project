877c03c16b4705f69b2e30d0e5bfc7b026a3eb50
Ty <tristianashbysanders@gmail.com>
2024-10-14 19:36:46 -0500

    removing carry over from local testing

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-const useSecureCookies = process.env.NEXT_PUBLIC_NEXTAUTH_URL.startsWith("http://");
+const useSecureCookies = process.env.NEXT_PUBLIC_NEXTAUTH_URL.startsWith("https://");
 const cookiePrefix = useSecureCookies ? "__Secure-" : "";
 const hostName = new URL(process.env.NEXT_PUBLIC_NEXTAUTH_URL).hostname;
 export const TOKEN_NAME = `${cookiePrefix}next-auth.session-token`; 
