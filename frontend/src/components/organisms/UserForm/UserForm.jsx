2247f21886bb5293db996da438e4e74129e0ecce
Anjie Zhou <anjie.zhou2003@gmail.com>
2024-10-28 14:37:05 -0500

    changed new member form email caption

--- frontend/src/components/organisms/UserForm/UserForm.jsx
+++ frontend/src/components/organisms/UserForm/UserForm.jsx
@@ -116,7 +116,7 @@ const UserForm = ({
               variant="caption"
               sx={{ marginLeft: "10px", color: "grey" }}
             >
-              {edit ? "Cannot be changed" : "Cannot be changed after submitted"}
+              {edit ? "Cannot be changed" : "Gmail only for sign-in. Cannot be changed after submitted."}
             </Typography>
           </CaptionBox>
         </SideBySideBox>
